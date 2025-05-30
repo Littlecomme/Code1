fba4039557436f37ad05e2ab47047046c2763c24
KabachkovayaIkra <pavlentijfedorenko@gmail.com>
2025-05-30 22:36:42 +0700

    Homework

--- ConsoleApplication7/ConsoleApplication7.cpp
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -19,27 +19,85 @@ void security() {
 
 }
 
-int main() {
 
-	std::string bot[3] {"k","n","b"};
-	int r = rand()% 3;
-    std::string a;
+void RSP() {
 
-	std::cout << "Enter b or k or n\n";
-	std::cin >> a;
+	while (true) {
 
-	if (a == "k" && bot[r] == "n" || a == "n" && bot[r] == "b" || a == "b" && bot[r] == "k") {
-		std::cout << "You win\n";
+		std::string bot[7]{ "Rock","Scissors","Paper", "rock", "scissors", "paper" };
+		int r = rand() % 3;
+		std::string a;
+
+
+		std::cout << "Enter Rock, Paper or Scissors\n";
+		std::cin >> a;
+
+		if ((a == "Rock" || a == "rock") && bot[r] == "Scissors" || (a == "Scissors" || a == "scissors") && bot[r] == "Paper" || (a == "Paper" || a == "paper") && bot[r] == "Rock") {
+			std::cout << "You win\n";
+		}
+		else if ((a == "Rock" || a == "rock") && bot[r] == "Paper" || (a == "Scissors" || a == "scissors") && bot[r] == "Rock" || (a == "Paper" || a == "paper") && bot[r] == "Scissors") {
+			std::cout << "You lose\n";
+		}
+		else if ((a == "Rock" || a == "rock") && bot[r] == "Rock" || (a == "Scissors" || a == "scissors") && bot[r] == "Scissors" || (a == "Paper" || a == "paper") && bot[r] == "Paper") {
+			std::cout << "Draw\n";
+		}
+		else {
+			std::cout << "Subject not found\n";
+		}
+
+		std::cout << bot[r];
+		std::cout << "\n";
 	}
-	else if (a == "k" && bot[r] == "b" || a == "n" && bot[r] == "k" || a == "b" && bot[r] == "n") {
-		std::cout << "You lose\n";
-	}
-	else if (a == "k" && bot[r] == "k" || a == "n" && bot[r] == "n" || a == "b" && bot[r] == "b") {
-		std::cout << "Draw\n";
-	}
-	std::cout << bot[r];
-		//ƒописать кнб и создать массив размером 4 (7 -2 4 -9), вывести его полностью, а потом вывести отриц числа
-		//*¬ывести числа в пор€дке возрастани€ (пузырьковый метод), смотреть в интернете, только, если не пойму
 
-	
 }
+
+void choosegame() {
+
+	std::string a1;
+	std::cin >> a1;
+
+	if (a1 == "RSP") {
+		RSP();
+	}
+	else if (a1 == "security") {
+		security();
+	}
+
+}
+
+void numbers() {
+
+	int c[4]{ 7, -2, 4, -9 };
+	for (int h = 0; h < 4; h++) {
+		std::cout << c[h] << " ";
+	}
+
+	std::cout << "\n";
+
+	for (int h = 0; h < 4; h++) {
+		if (c[h] < 0) {
+			std::cout << c[h] << " ";
+		}
+	}
+
+	std::cout << "\n";
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = i + 1; j < 4; j++) {
+			if (c[i] > c[j]) {
+				std::swap(c[j], c[i]);
+
+			}
+		}
+
+	}
+	for (int h = 0; h < 4; h++) {
+		std::cout << c[h] << " ";
+	}
+	//std::cout << c[1] << " " << c[3];
+
+ }
+
+
+int main() {}
