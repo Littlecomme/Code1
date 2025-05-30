1e05929ceaf270383b7984d8027afe0284a5a34b
pavel <pavlentijfedorenko@gmail.com>
2025-05-30 18:17:56 +0700

    knb done

--- ConsoleApplication7/ConsoleApplication7.cpp
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -25,10 +25,19 @@ int main() {
 	int r = rand()% 3;
     std::string a;
 
+	std::cout << "Enter b or k or n\n";
 	std::cin >> a;
 
-	if (a == "k" && bot[r] == "n" || a == "n" && bot[r] == "b" || a == "b" && bot[r] == "k")
-
+	if (a == "k" && bot[r] == "n" || a == "n" && bot[r] == "b" || a == "b" && bot[r] == "k") {
+		std::cout << "You win\n";
+	}
+	else if (a == "k" && bot[r] == "b" || a == "n" && bot[r] == "k" || a == "b" && bot[r] == "n") {
+		std::cout << "You lose\n";
+	}
+	else if (a == "k" && bot[r] == "k" || a == "n" && bot[r] == "n" || a == "b" && bot[r] == "b") {
+		std::cout << "Draw\n";
+	}
+	std::cout << bot[r];
 		//ƒописать кнб и создать массив размером 4 (7 -2 4 -9), вывести его полностью, а потом вывести отриц числа
 		//*¬ывести числа в пор€дке возрастани€ (пузырьковый метод), смотреть в интернете, только, если не пойму
 
